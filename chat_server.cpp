7fba9ced0887ba109806b3333e5253deee58f528
Matt Williams <Mattwilliams1515@gmail.com>
2018-11-13 21:33:06 -0600

    const

--- chat_server.cpp
+++ chat_server.cpp
@@ -86,9 +86,9 @@ private:
   void do_read_json() {
     auto self(shared_from_this());
     std::cout << "trying to read!" << std::endl;
-    boost::asio::mutable_buffer recv_buff;
+    const boost::asio::mutable_buffer recv_buff;
     std::cout << "uhhh" << std::endl;
-    boost::asio::async_read(socket_, boost::asio::buffer(const boost::asio::mutable_buffer &recv_buff), [this, self, recv_buff](std::error_code ec, std::size_t /*length*/) {
+    boost::asio::async_read(socket_, boost::asio::buffer(&recv_buff), [this, self, recv_buff](std::error_code ec, std::size_t /*length*/) {
       std::cout << "Hey im here" << std::endl;
       if (!ec) {
         std::cout << "Okay lets try something." << std::endl;
