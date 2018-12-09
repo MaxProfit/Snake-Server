13f14891bd70875f24f65b55af104aa940d55a85
Matt Williams <Mattwilliams1515@gmail.com>
2018-12-09 00:25:32 -0600

    Format changes. Removed main from the implementation

--- client/src/chat_client.cpp
+++ client/src/chat_client.cpp
@@ -1,19 +1,19 @@
 #include "../include/chat_client.hpp"
 
 using boost::asio::ip::tcp;
-
 using nlohmann::json;
 
 // Code below derived using https://goo.gl/XHuJZL
 
-chat_client::chat_client(boost::asio::io_context& io_context, 
+chat_client::chat_client(
+  boost::asio::io_context& io_context, 
   const tcp::resolver::results_type& endpoints) 
-  : io_context_(io_context), socket_(io_context) {
+    : io_context_(io_context), socket_(io_context) {
       do_connect(endpoints);
 }
 
 void chat_client::close() {
-  boost::asio::post(io_context_, [this]() { socket_.close(); });
+  boost::asio::post(io_context_, [this](){ socket_.close(); });
 }
 
 json chat_client::get_recent_json() {
@@ -49,16 +49,20 @@ void chat_client::write(const chat_message& msg) {
 }
 
 void chat_client::do_connect(const tcp::resolver::results_type& endpoints) {
-  boost::asio::async_connect(socket_, endpoints,
+  boost::asio::async_connect(
+    socket_,
+    endpoints,
     [this](boost::system::error_code ec, tcp::endpoint) {
       if (!ec) {
         do_read_header();
       }
-  });
+    }
+  );
 }
 
 void chat_client::do_read_header() {
-  boost::asio::async_read(socket_,
+  boost::asio::async_read(
+    socket_,
     boost::asio::buffer(read_msg_.data(), chat_message::header_length),
     [this](boost::system::error_code ec, std::size_t /*length*/) {
       if (!ec && read_msg_.decode_header()) {
@@ -66,38 +70,43 @@ void chat_client::do_read_header() {
       } else {
         socket_.close();
       }
-    });
+    }
+  );
 }
 
 void chat_client::do_read_body() {
-  boost::asio::async_read(socket_,
-      boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
-      [this](boost::system::error_code ec, std::size_t /*length*/) {
-        if (!ec) {
-          // std::cout.write(read_msg_.body(), read_msg_.body_length());
-          // std::cout << "\n";
-          json_recieved_from_server_ = convert_to_json(read_msg_.body());
-          do_read_header();
-        } else {
-          socket_.close();
-        }
-      });
+  boost::asio::async_read(
+    socket_,
+    boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+    [this](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec) {
+        // std::cout.write(read_msg_.body(), read_msg_.body_length());
+        // std::cout << "\n";
+        json_recieved_from_server_ = convert_to_json(read_msg_.body());
+        do_read_header();
+      } else {
+        socket_.close();
+      }
+    }
+  );
 }
 
 void chat_client::do_write() {
-  boost::asio::async_write(socket_,
-      boost::asio::buffer(write_msgs_.front().data(),
-        write_msgs_.front().length()),
-      [this](boost::system::error_code ec, std::size_t /*length*/) {
-        if (!ec) {
-          write_msgs_.pop_front();
-          if (!write_msgs_.empty()) {
-            do_write();
-          }
-        } else {
-          socket_.close();
+  boost::asio::async_write(
+    socket_,
+    boost::asio::buffer(write_msgs_.front().data(),
+    write_msgs_.front().length()),
+    [this](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec) {
+        write_msgs_.pop_front();
+        if (!write_msgs_.empty()) {
+          do_write();
         }
-      });
+      } else {
+        socket_.close();
+      }
+    }
+  );
 }
 
 json chat_client::convert_to_json(const char* const to_become_json) {
@@ -109,34 +118,4 @@ json chat_client::convert_to_json(const char* const to_become_json) {
 std::string chat_client::convert_from_json(json to_become_string) {
   // Dumps the serialized json to string
   return to_become_string.dump();
-}
-
-int main() {
-  try {
-    boost::asio::io_context io_context;
-
-    const std::string kADDRESS("54.237.158.244");
-    const std::string kPORT("49145");
-    tcp::resolver resolver(io_context);
-    auto endpoints = resolver.resolve(kADDRESS.c_str(), kPORT.c_str());
-    chat_client c(io_context, endpoints);
-
-    std::thread t([&io_context](){ io_context.run(); });
-  
-    auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
-    c.send_json(j3);
-
-    sleep(5);
-
-    std::cout << c.get_recent_json().dump() << std::endl;
-
-    c.close();
-    t.join();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
 }
--- server/src/chat_server.cpp
+++ server/src/chat_server.cpp
@@ -1,7 +1,6 @@
 #include "../include/chat_server.hpp"
 
 using boost::asio::ip::tcp;
-
 using nlohmann::json;
 
 //----------------------------------------------------------------------
@@ -11,13 +10,12 @@ chat_participant::~chat_participant() {}
 //----------------------------------------------------------------------
 // Chat Room
 
-  // Code below derived from https://goo.gl/xuDdLC
+// Code below derived from https://goo.gl/xuDdLC
 
 void chat_room::join(chat_participant_ptr participant) {
   participants_.insert(participant);
   // Send the init messages to the client, giving them a place or whatever
   // Send the current gameboard state
-
 }
 
 void chat_room::leave(chat_participant_ptr participant) {
@@ -65,64 +63,71 @@ void chat_session::deliver(const chat_message& msg) {
 
 void chat_session::do_read_header() {
   auto self(shared_from_this());
-  boost::asio::async_read(socket_,
-      boost::asio::buffer(read_msg_.data(), chat_message::header_length),
-      [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-        if (!ec && read_msg_.decode_header()) {
-          do_read_body();
-        } else {
-          room_.leave(shared_from_this());
-          }
-      });
+  boost::asio::async_read(
+    socket_,
+    boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec && read_msg_.decode_header()) {
+        do_read_body();
+      } else {
+        room_.leave(shared_from_this());
+      }
+    }
+  );
 }
 
 void chat_session::do_read_body() {
   auto self(shared_from_this());
-  boost::asio::async_read(socket_,
-      boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
-      [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-        if (!ec) {
-          // Parses the string into json, then adds it to the vector
-          std::string std_string(read_msg_.body());
-          room_.add_to_json_vec(json::parse(std_string));
-          // TODO: make a std::pair with the json obj, pointer number to verify
-          // the id of the person sending the information
+  boost::asio::async_read(
+    socket_,
+    boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec) {
+        // Parses the string into json, then adds it to the vector
+        std::string std_string(read_msg_.body());
+        room_.add_to_json_vec(json::parse(std_string));
+        // TODO: make a std::pair with the json obj, pointer number to verify
+        // the id of the person sending the information
 
-          std::cout << "I received something!!" << std::endl;
-          std::cout.write(read_msg_.body(), read_msg_.body_length());
-          std::cout << std::endl;
-          room_.deliver(read_msg_);
-          do_read_header();
-        } else {
-          room_.leave(shared_from_this());
-        }
-      });
+        std::cout << "I received something!!" << std::endl;
+        std::cout.write(read_msg_.body(), read_msg_.body_length());
+        std::cout << std::endl;
+        room_.deliver(read_msg_);
+        do_read_header();
+      } else {
+        room_.leave(shared_from_this());
+      }
+    }
+  );
 }
 
 void chat_session::do_write() {
   auto self(shared_from_this());
-  boost::asio::async_write(socket_,
-      boost::asio::buffer(write_msgs_.front().data(),
-        write_msgs_.front().length()),
-      [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-        if (!ec) {
-          write_msgs_.pop_front();
-          if (!write_msgs_.empty()) {
-            do_write();
-          }
-        } else {
-          room_.leave(shared_from_this());
+  boost::asio::async_write(
+    socket_,
+    boost::asio::buffer(write_msgs_.front().data(),
+    write_msgs_.front().length()),
+    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec) {
+        write_msgs_.pop_front();
+        if (!write_msgs_.empty()) {
+          do_write();
         }
-      });
+      } else {
+        room_.leave(shared_from_this());
+      }
+    }
+  );
 }
 
 //----------------------------------------------------------------------
 // Chat Server
 
-chat_server::chat_server( boost::asio::io_context& io_context, 
-                          const tcp::endpoint& endpoint) 
-                          : acceptor_(io_context, endpoint) {
-  do_accept();
+chat_server::chat_server(
+  boost::asio::io_context& io_context, 
+  const tcp::endpoint& endpoint) 
+    : acceptor_(io_context, endpoint) {
+      do_accept();
 }
 
 std::vector<json> chat_server::get_json_vector() {
@@ -150,44 +155,14 @@ void chat_server::send_json(json json_to_send) {
 }
 
 void chat_server::do_accept() {
-  acceptor_.async_accept([this](boost::system::error_code ec,
-                                tcp::socket socket) {
-    if (!ec) {
-      std::make_shared<chat_session>(std::move(socket), room_)->start();
+  acceptor_.async_accept([this](
+    boost::system::error_code ec, 
+    tcp::socket socket) {
+      if (!ec) {
+        std::make_shared<chat_session>(std::move(socket), room_)->start();
+      }
+
+      do_accept();
     }
-
-    do_accept();
-  });
-}
-
-//----------------------------------------------------------------------
-
-int main() {
-  try {
-
-    boost::asio::io_context io_context;
-
-    // This work object keeps the thread 
-    // alive and not quitting on us prematurely
-    boost::asio::io_context::work work(io_context);
-
-    const uint16_t kPORT {49145};
-    tcp::endpoint endpoint(tcp::v4(), kPORT);
-    chat_server server(io_context, endpoint);
-
-    // Creates a new thread so we can do other computations while this runs
-    std::thread thread([&io_context](){ io_context.run(); });
-
-    // The code will continue to run forever because the server always waiting
-    // for more connections, it will not stop until the program is exited
-    sleep(4);
-
-    server.send_json(json::parse("{ \"happy\": nah, \"pi\": 3 }"));
-
-    thread.join();
-  } catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+  );
 }
